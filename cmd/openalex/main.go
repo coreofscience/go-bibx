2bd9a06bcc9bc40fcf113319457242ed879b98c6
Oscar Arbelaez <oscar@arbelaez.dev>
2026-05-05 00:18:06 +0100

    chore: wire up sqlite-vec

--- cmd/openalex/main.go
+++ cmd/openalex/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -9,6 +10,9 @@ import (
 
 	"github.com/coreofscience/go-bibx/sources"
 	"github.com/urfave/cli/v3"
+
+	sqlite_vec "github.com/asg017/sqlite-vec-go-bindings/cgo"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -33,6 +37,23 @@ func main() {
 			},
 		},
 		Action: func(ctx context.Context, c *cli.Command) error {
+			sqlite_vec.Auto()
+
+			db, err := sql.Open("sqlite3", ":memory:")
+			if err != nil {
+				slog.Error("failed to open database", "error", err)
+				return err
+			}
+			defer db.Close()
+
+			var vecVersion string
+			err = db.QueryRow("select vec_version()").Scan(&vecVersion)
+			if err != nil {
+				slog.Error("failed to get vec version", "error", err)
+				return err
+			}
+			slog.Info("vec loaded", "version", vecVersion)
+
 			logLevel := slog.LevelInfo
 			if c.Bool("verbose") {
 				logLevel = slog.LevelDebug
