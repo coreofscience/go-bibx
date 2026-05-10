575224a8dbcd07b6829c512229fa85379b1babca
Oscar Arbelaez <oscar@arbelaez.dev>
2026-05-10 12:59:30 +0100

    feat: improve visualisation

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -1,25 +1,18 @@
 package analysis
 
 import (
+	"github.com/coreofscience/go-bibx/algorithms"
 	"github.com/coreofscience/go-bibx/articles"
 	"github.com/coreofscience/go-bibx/collection"
 )
 
-type Category string
-
-const (
-	CategoryRoot  Category = "root"
-	CategoryTrunk Category = "trunk"
-	CategoryLeaf  Category = "leaf"
-)
-
 type Node struct {
-	ID       string            `json:"id"`
-	Category Category          `json:"category"`
-	Root     float64           `json:"root"`
-	Trunk    float64           `json:"trunk"`
-	Leaf     float64           `json:"leaf"`
-	Article  *articles.Article `json:"article"`
+	ID        string              `json:"id"`
+	Category  algorithms.Category `json:"category"`
+	Rootness  float64             `json:"rootness"`
+	Trunkness float64             `json:"trunkness"`
+	Leafness  float64             `json:"leafness"`
+	Article   *articles.Article   `json:"article"`
 }
 
 type Link struct {
@@ -34,14 +27,25 @@ type Analysis struct {
 
 func New(c *collection.Collection) *Analysis {
 	nodes := make([]*Node, 0, c.Len())
+	graph, err := c.CitationGraph()
+	if err != nil {
+		return nil
+	}
+	sap := algorithms.NewSap(graph)
+	result := sap.Run()
 	for article := range c.All() {
 		articleKey := article.Key()
 		if articleKey == nil {
 			continue
 		}
+		key := *articleKey
 		nodes = append(nodes, &Node{
-			ID:      *articleKey,
-			Article: article,
+			ID:        key,
+			Article:   article,
+			Category:  result.Categories[key],
+			Rootness:  result.Rootness[key],
+			Trunkness: result.Trunkness[key],
+			Leafness:  result.Leafness[key],
 		})
 	}
 	links := make([]*Link, 0, c.Len())
--- internal/commands/view/view.go
+++ internal/commands/view/view.go
@@ -49,13 +49,16 @@ func New() *cli.Command {
 			http.HandleFunc("GET /collection.json.gz", func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.Header().Set("Content-Encoding", "gzip")
+				w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
+				w.Header().Set("Pragma", "no-cache")
+				w.Header().Set("Expires", "0")
 				http.ServeFile(w, r, file)
 			})
 			http.HandleFunc("GET /events", func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "text/event-stream")
 				w.Header().Set("Cache-Control", "no-cache")
 				w.Header().Set("Connection", "keep-alive")
-				w.Header().Set("X-Accel-Buffering", "no") // Disables buffering in Nginx/proxies
+				w.Header().Set("X-Accel-Buffering", "no")
 				w.Header().Set("Cache-Control", "no-cache")
 
 				// Just keep the connection open.
