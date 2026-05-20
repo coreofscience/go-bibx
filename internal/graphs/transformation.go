13f8b37126088f04b2dfe719b818026bf84d5757
Oscar Arbelaez <oscar@arbelaez.dev>
2026-05-20 21:35:22 +0100

    chore: fix potential bug

--- internal/graphs/transformation.go
+++ internal/graphs/transformation.go
@@ -77,7 +77,7 @@ func Keep[V comparable](g gograph.Graph[V], toKeep []V) (gograph.Graph[V], error
 		}
 		source := gograph.NewVertex(sourceLabel)
 		dest := gograph.NewVertex(destLabel)
-		if edge := newGraph.GetEdge(dest, source); edge != nil {
+		if edge := newGraph.GetEdge(source, dest); edge != nil {
 			continue
 		}
 		_, err := newGraph.AddEdge(source, dest, gograph.WithEdgeWeight(edge.Weight()))
